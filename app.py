b30ee4ae5aee947362aacc1621c59bcf4159b9b5
yoplenople <junyboy124@gmail.com>
2024-11-18 14:07:01 +0900

    index.html 추가

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def index():
-    return "Hello, World!"
+def home():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
